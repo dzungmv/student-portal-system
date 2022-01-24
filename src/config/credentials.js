991fb628ef922265f30166d7bec259e9f003b8d4
Dũng Minh <jungjung.now@gmail.com>
2022-01-24 19:42:38 +0700

    add file

---
+++ src/config/credentials.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "cookieSecret": "kghkasfcgnasuyg6526as",
+    "mongo": {
+        "connectionString": "mongodb+srv://vinhtieng:vinhtieng@cluster0.ugswm.mongodb.net/AdvancedWeb2122?retryWrites=true&w=majority"
+    },
+    "google": {
+        "clientID": "59826905681-qoelvu7i69m06fhbut2g70fkgehoime6.apps.googleusercontent.com",
+        "clientSecret": "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",
+        "callbackURL": "https://advanced-web-tdtu-2021-2022.herokuapp.com/google/callback"
+    },
+    "cloudinary": {
+        "cloud_name": "nguyenvinhtieng",
+        "api_key": "759141759622245",
+        "api_secret": "yEjkljPOCBHmxA2P2v7kLUdX0Ho"
+    }
+}
